abff62211016deaab2e9f4aa2ee81337b5d24eba
Karol Baczewski <braxnu@gmail.com>
2022-10-16 21:10:10 +0000

    logs & final script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -7,15 +7,24 @@ set -e
 # Step 1:
 # This is your Docker ID/path
 TAG=braxnu/uda-kube
+DEPL_NAME=uda-py-depl
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
-kubectl create deployment my-dep --image=$TAG
+kubectl delete service $DEPL_NAME
+kubectl delete deployment $DEPL_NAME
+
+kubectl create deployment $DEPL_NAME --image=$TAG -r 4
 
 # Step 3:
 # List kubernetes pods
 
+kubectl get pods
+
 # Step 4:
 # Forward the container port to a host
 
+kubectl expose deployment $DEPL_NAME --port=80
+
+kubectl port-forward services/$DEPL_NAME 8080:80
